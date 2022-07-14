bfe80b1d63d2a38a7bf76a1bf1ee959c61d592e1
gdogjam <griffin@teksmith.com>
2022-07-14 18:01:43 -0400

    Mod tutorial part 2 done

--- src/main/java/net/gdogjam/tutorialmod/TutorialMod.java
+++ src/main/java/net/gdogjam/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.gdogjam.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.gdogjam.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+
+		ModItems.registerModItems();
 	}
 }
