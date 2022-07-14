b450fef1d6299d7ac610b434168e1fe8184e6cfd
gdogjam <griffin@teksmith.com>
2022-07-14 19:01:49 -0400

    custom block tutorial finished

--- src/main/java/net/gdogjam/tutorialmod/TutorialMod.java
+++ src/main/java/net/gdogjam/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.gdogjam.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.gdogjam.tutorialmod.Block.ModBlocks;
 import net.gdogjam.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,5 +16,6 @@ public class TutorialMod implements ModInitializer {
 
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
