4d484ad29d1d2bad267d3cf3dba6431130f20e12
gdogjam <griffin@teksmith.com>
2022-07-15 11:13:35 -0400

    mineable blocks

---
+++ src/main/java/net/gdogjam/tutorialmod/item/custom/EightBallItem.java
@@ -0,0 +1,10 @@
+package net.gdogjam.tutorialmod.item.custom;
+
+import net.minecraft.item.Item;
+
+public class EightBallItem extends Item {
+    public EightBallItem(Settings settings) {
+        super(settings);
+    }
+
+}
