49e8224ea3a5c2520d3992ca663da63cff86c396
gdogjam <griffin@teksmith.com>
2022-07-15 14:26:39 -0400

    tooltips

--- src/main/java/net/gdogjam/tutorialmod/item/custom/EightBalIItem.java
+++ src/main/java/net/gdogjam/tutorialmod/item/custom/EightBalIItem.java
@@ -1,13 +1,19 @@
 package net.gdogjam.tutorialmod.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBalIItem extends Item {
     public EightBalIItem(Settings settings) {
@@ -24,6 +30,17 @@ public class EightBalIItem extends Item {
         }
 
         return super.use(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world,
+                              List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right click to get a random number").formatted(Formatting.AQUA));
+        }else{
+            tooltip.add(Text.literal("Press Shift for more info!").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
     }
 
     private void outputRandomNumber(PlayerEntity player){
