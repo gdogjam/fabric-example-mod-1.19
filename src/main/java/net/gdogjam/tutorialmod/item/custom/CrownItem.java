06ccc52b101a19c5066b8426bc00314fd526dee4
gdogjam <griffin@teksmith.com>
2022-07-18 14:59:12 -0400

    working on crown

---
+++ src/main/java/net/gdogjam/tutorialmod/item/custom/CrownItem.java
@@ -0,0 +1,54 @@
+package net.gdogjam.tutorialmod.item.custom;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class CrownItem extends Item {
+    public CrownItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if(!world.isClient() && hand == Hand.MAIN_HAND){
+            //output number
+            user.sendMessage(Text.literal("It's good to be King!"));
+            user.
+            // add a cooldown
+            user.getItemCooldownManager().set(this,20);
+        }
+
+        return super.use(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world,
+                              List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right click to get a random number").formatted(Formatting.AQUA));
+        }else{
+            tooltip.add(Text.literal("Press Shift for more info!").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
+    private void outputRandomNumber(PlayerEntity player){
+        player.sendMessage(Text.literal("Your Number is " + getRandomNumber()));
+    }
+
+    private int getRandomNumber(){
+        return Random.createLocal().nextInt(10);
+    }
+}
