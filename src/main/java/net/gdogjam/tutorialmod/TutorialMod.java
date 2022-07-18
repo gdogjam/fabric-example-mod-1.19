50597af90b4684efc3ee98dd2fbab0458494b1bc
gdogjam <griffin@teksmith.com>
2022-07-17 21:39:31 -0400

    eggplant bug fixed

--- src/main/java/net/gdogjam/tutorialmod/TutorialMod.java
+++ src/main/java/net/gdogjam/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.gdogjam.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 import net.gdogjam.tutorialmod.Block.ModBlocks;
 import net.gdogjam.tutorialmod.item.ModItems;
+import net.gdogjam.tutorialmod.villager.ModVillagers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,5 +18,7 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+		ModVillagers.registerVillagers();
+		ModVillagers.registerTrades();
 	}
 }
