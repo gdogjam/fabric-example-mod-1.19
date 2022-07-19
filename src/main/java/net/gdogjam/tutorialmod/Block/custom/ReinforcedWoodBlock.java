74b334f2fa06b7f602afe9d7fffd2b02dfeb9cf3
gdogjam <griffin@teksmith.com>
2022-07-19 14:25:39 -0400

    tags world gen

---
+++ src/main/java/net/gdogjam/tutorialmod/Block/custom/ReinforcedWoodBlock.java
@@ -0,0 +1,41 @@
+package net.gdogjam.tutorialmod.Block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+public class ReinforcedWoodBlock extends Block {
+    public ReinforcedWoodBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player,
+                              Hand hand, BlockHitResult hit) {
+        if(!world.isClient() && hand == Hand.MAIN_HAND) {
+            //Server: Main hand & off hand
+            //client: Main hand & off hand
+            player.sendMessage(Text.literal("Right Clicked This!"));
+        }
+
+
+        return super.onUse(state, world, pos, player, hand, hit);
+    }
+
+    @Override
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+        super.onBroken(world, pos, state);
+    }
+
+}
